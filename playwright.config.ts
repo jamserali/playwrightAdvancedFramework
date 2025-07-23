7cd143565f6e0b203ea389f976f7f84f2ed67e04
Jamser Ali <jamserali0011@gmail.com>
2025-07-23 14:10:32 +0530

    workers added

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,13 +16,12 @@ module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 8 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { outputFolder: 'result', open: 'never' }],
               ['allure-playwright'],
